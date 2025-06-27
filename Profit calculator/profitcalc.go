819931cb72eca62dfdc1b10a99eee3c40d800dda
michael-muller-tech <github@mm-ul.xyz>
2025-06-28 00:27:15 +0100

    Changed the way variables are declared to make code more concise.

--- Profit calculator/profitcalc.go
+++ Profit calculator/profitcalc.go
@@ -11,11 +11,7 @@ func main() {
 
 func profitCalc() {
 	//Variable declaration
-	var revenue float64
-	var expenses float64
-	var taxRate float64
-	var EBT float64 //earnings before tax
-	var profit float64
+	var revenue, expenses, taxRate float64
 
 	//Actual function
 	fmt.Print("What is your expected revenue? ")
@@ -28,8 +24,8 @@ func profitCalc() {
 	fmt.Scan(&taxRate)
 
 	//Calculation
-	EBT = revenue-expenses
-	profit = (revenue-expenses)*((100-taxRate)/100)
+	EBT := revenue-expenses
+	profit := (revenue-expenses)*((100-taxRate)/100)
 	
 	fmt.Println("Revenue entered: ", revenue)
 	fmt.Println("Expenses entered: ", expenses)
